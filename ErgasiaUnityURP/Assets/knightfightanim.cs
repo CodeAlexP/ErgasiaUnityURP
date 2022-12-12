a8cb8614eace239aae0a71ae056e76bad6533408
GIANNIS25A <giannis25al@gmail.com>
2022-12-12 17:55:12 +0200

    added new character + press "y" for die animation

---
+++ ErgasiaUnityURP/Assets/knightfightanim.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knightfightanim : MonoBehaviour
+{
+    
+    public GameObject Knight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("y")){
+                Knight.GetComponent<Animator>().Play("death");
+        }
+        
+    }
+}
