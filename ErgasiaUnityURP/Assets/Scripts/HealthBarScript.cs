ca9d76ac6b732139318841e245a681e2b9b1f016
Trapilaos <nnnikospagidas@gmail.com>
2023-03-03 00:00:46 +0200

    Health Bar
    Added a basic Health Bar that takes 20 damage by pressing Space

---
+++ ErgasiaUnityURP/Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+}
