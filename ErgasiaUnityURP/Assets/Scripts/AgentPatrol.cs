fc1bb4206b39c2b877eac99366096fd930052a17
GIANNIS25A <giannis25al@gmail.com>
2023-03-20 11:54:31 +0200

    Added Comments to AgentPatrol + minimap script
    apo comments exoun meinei mono tou nikou diladi to healtbar kai to player

--- ErgasiaUnityURP/Assets/Scripts/AgentPatrol.cs
+++ ErgasiaUnityURP/Assets/Scripts/AgentPatrol.cs
@@ -10,53 +10,47 @@ public class AgentPatrol : MonoBehaviour
     Animator animator;
     [SerializeField] private Transform[] points;
     int pointIndex;
-    bool walking = false;
+    
     bool waiting = false;
 
     // Start is called before the first frame update
-    void Start()
+   // Start is called before the first frame update.
+void Start()
+{
+    agent = GetComponent<NavMeshAgent>(); // Get a reference to the NavMeshAgent component.
+    animator = GetComponent<Animator>(); // Get a reference to the Animator component.
+
+    pointIndex = -1; // Initialize the patrol point index to -1.
+  
+
+    StartCoroutine(NextPoint()); // Start the patrol coroutine.
+}
+
+// Update is called once per frame.
+void Update()
+{
+    // Check if the agent has reached the current patrol point and is not waiting or has no more path to follow.
+    if (agent.remainingDistance <= agent.stoppingDistance && !waiting && !agent.pathPending)
     {
-        agent = GetComponent<NavMeshAgent>();
-        
-        
-
-        pointIndex = -1;
-        walking = false;
-       // animator.SetBool("Walking", false);
-        StartCoroutine(NextPoint());
+        StartCoroutine(NextPoint()); // Start the coroutine to go to the next patrol point.
     }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (agent.remainingDistance <= agent.stoppingDistance && !waiting && !agent.pathPending)
-        {
-        
-               
-            StartCoroutine(NextPoint());
-           
-           
-        }
-    }
+// Coroutine to move the agent to the next patrol point.
+IEnumerator NextPoint()
+{
+    animator.Play("Idle Walk Run Blend"); // Play the animation for the idle to walk transition.
 
-    IEnumerator NextPoint()
-    {
-         GetComponent<Animator>().Play("Idle Walk Run Blend");
+    float randomSeconds = Random.Range(1, 4); // Generate a random wait time between 1 and 4 seconds.
+    yield return new WaitForSeconds(randomSeconds); // Wait for the random time.
 
-        float randomSeconds = Random.Range(1, 4);
-        yield return new WaitForSeconds(randomSeconds);
+    pointIndex = (pointIndex + 1) % points.Length; // Increment the patrol point index to the next point.
+    agent.SetDestination(points[pointIndex].position); // Set the agent's destination to the next patrol point.
 
-        pointIndex = (pointIndex + 1) % points.Length;
-        agent.SetDestination(points[pointIndex].position);
+    animator.Play("Walking"); // Play the animation for walking.
 
-              
-         GetComponent<Animator>().Play("Walking");
-
-      
-        
-
-        yield return null;
-    }
+    yield return null; // Return null to exit the coroutine.
+}
 }
 
 
--- ErgasiaUnityURP/Assets/Scripts/Minimap.cs
+++ ErgasiaUnityURP/Assets/Scripts/Minimap.cs
@@ -11,18 +11,22 @@ public class Minimap: MonoBehaviour
     private Transform cameraTransform;
     private Quaternion initialRotation;
 
-    void Start()
-    {
-        cameraTransform = Camera.main.transform;
-        initialRotation = transform.rotation;
-    }
+void Start()
+{
+    // Get the transform of the main camera and the initial rotation of the minimap.
+    cameraTransform = Camera.main.transform;
+    initialRotation = transform.rotation;
+}
 
-    void Update()
-    {
-        float angle = cameraTransform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, 0, -angle);
-        transform.rotation = initialRotation * rotation;
-    }
+void Update()
+{
+    // Get the y-axis rotation of the camera and create a new quaternion rotation to apply to the minimap.
+    float angle = cameraTransform.eulerAngles.y;
+    Quaternion rotation = Quaternion.Euler(0, 0, -angle);
+
+    // Set the rotation of the minimap to the initial rotation plus the new camera rotation.
+    transform.rotation = initialRotation * rotation;
+}
 }
 
 
