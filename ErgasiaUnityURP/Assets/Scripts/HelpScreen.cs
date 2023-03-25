0faf8be6bd022b2286f81e6908e1702540679ba0
Trapilaos <nnnikospagidas@gmail.com>
2023-03-25 20:20:40 +0200

    Revert
    This reverts commit 528128d2c0157cdfa29cc52fd0b7ec6630b9e92e.

--- ErgasiaUnityURP/Assets/Scripts/HelpScreen.cs
+++ ErgasiaUnityURP/Assets/Scripts/HelpScreen.cs
@@ -7,6 +7,9 @@ public class HelpScreen : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject helpScreenUI;
+    public GameObject Minimap;
+     public GameObject Healtbar;
+    
     [SerializeField] CinemachineBrain cinemachineBrain;
     // Update is called once per frame
     void Update()
@@ -28,9 +31,13 @@ public class HelpScreen : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked; // Make the cursor visible and center it
         cinemachineBrain.enabled = !GameIsPaused; // Unpause the in game movement
         Time.timeScale = 1f; // Unfreeze all of the animations
+        Minimap.SetActive(true);
+        Healtbar.SetActive(true);
     }
 
     void Pause() {
+         Minimap.SetActive(false);
+        Healtbar.SetActive(false);
         helpScreenUI.SetActive(true); // Make the pause menu visible
         GameIsPaused = true; // Make GameIsPaused true so that if the user presses the F1 button it will unpause
         Cursor.visible = true; // Make the cursor visible so that the user can press the buttons
